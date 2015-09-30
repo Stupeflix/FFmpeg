6241bea3b3951fe0a08a1b60d3bea27f870612b6
Matthieu Bouron <matthieu.bouron@stupeflix.com>
2015-09-30 18:26:56 +0200

    lavf: add Android content resolver protocol support
    Handles uri starting with content://.

---
+++ libavformat/android_content.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2015-2016 Matthieu Bouron <matthieu.bouron stupeflix.com>
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef AVFORMAT_ANDROID_CONTENT_H
+#define AVFORMAT_ANDROID_CONTENT_H
+
+void av_android_content_set_app_ctx(void *ctx);
+void *av_android_content_get_app_ctx(void);
+
+#endif /* AVFORMAT_ANDROID_CONTENT_H */
