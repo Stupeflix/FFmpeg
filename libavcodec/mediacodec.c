7b96584047a369855e28e9a02c6f359024993562
Matthieu Bouron <matthieu.bouron@stupeflix.com>
2016-03-29 22:30:36 +0200

    lavc/mediacodec: add surface wrapper

--- libavcodec/mediacodec.c
+++ libavcodec/mediacodec.c
@@ -29,6 +29,8 @@
 #if CONFIG_MEDIACODEC
 
 #include <jni.h>
+#include <pthread.h>
+#include <time.h>
 
 #include "libavcodec/avcodec.h"
 #include "libavutil/mem.h"
@@ -98,6 +100,392 @@ int av_mediacodec_release_buffer(AVMediaCodecBuffer *buffer, int render)
     return 0;
 }
 
+struct JNIAndroidSurfaceFields {
+
+    jclass surface_class;
+    jmethodID surface_init_id;
+    jmethodID surface_release_id;
+
+    jclass surface_texture_class;
+    jmethodID surface_texture_init_id;
+    jmethodID surface_texture_init2_id;
+    jmethodID attach_to_gl_context_id;
+    jmethodID detach_from_gl_context_id;
+    jmethodID update_tex_image_id;
+    jmethodID set_on_frame_available_listener_id;
+    jmethodID set_on_frame_available_listener2_id;
+    jmethodID get_transform_matrix_id;
+    jmethodID set_default_buffer_size_id;
+    jmethodID surface_texture_release_id;
+
+} JNIAndroidSurfaceFields;
+
+static const struct FFJniField jfields_mapping[] = {
+    { "android/view/Surface", NULL, NULL, FF_JNI_CLASS, offsetof(struct JNIAndroidSurfaceFields, surface_class), 1 },
+        { "android/view/Surface", "<init>", "(Landroid/graphics/SurfaceTexture;)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, surface_init_id), 1 },
+        { "android/view/Surface", "release", "()V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, surface_release_id), 1 },
+
+    { "android/graphics/SurfaceTexture", NULL, NULL, FF_JNI_CLASS, offsetof(struct JNIAndroidSurfaceFields, surface_texture_class), 1 },
+        { "android/graphics/SurfaceTexture", "<init>", "(I)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, surface_texture_init_id), 1 },
+        { "android/graphics/SurfaceTexture", "<init>", "(IZ)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, surface_texture_init2_id), 0 },
+        { "android/graphics/SurfaceTexture", "attachToGLContext", "(I)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, attach_to_gl_context_id), 1 },
+        { "android/graphics/SurfaceTexture", "detachFromGLContext", "()V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, detach_from_gl_context_id), 1 },
+        { "android/graphics/SurfaceTexture", "updateTexImage", "()V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, update_tex_image_id), 1 },
+        { "android/graphics/SurfaceTexture", "getTransformMatrix", "([F)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, get_transform_matrix_id), 1 },
+        { "android/graphics/SurfaceTexture", "setDefaultBufferSize", "(II)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, set_default_buffer_size_id), 1 },
+        { "android/graphics/SurfaceTexture", "setOnFrameAvailableListener", "(Landroid/graphics/SurfaceTexture$OnFrameAvailableListener;)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, set_on_frame_available_listener_id), 1 },
+        { "android/graphics/SurfaceTexture", "setOnFrameAvailableListener", "(Landroid/graphics/SurfaceTexture$OnFrameAvailableListener;Landroid/os/Handler;)V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, set_on_frame_available_listener2_id), 0 },
+        { "android/graphics/SurfaceTexture", "release", "()V", FF_JNI_METHOD, offsetof(struct JNIAndroidSurfaceFields, surface_texture_release_id), 1 },
+
+
+    { NULL }
+};
+
+typedef struct AndroidSurface {
+    const AVClass *class;
+    struct JNIAndroidSurfaceFields jfields;
+    jobject *surface;
+    jobject *surface_texture;
+    jobject *listener;
+    pthread_mutex_t lock;
+    pthread_cond_t cond;
+    int tex_id;
+    int on_frame_available;
+} AndroidSurface;
+
+static const AVClass android_surface_class = {
+    .class_name = "android_surface",
+    .item_name  = av_default_item_name,
+    .version    = LIBAVCODEC_VERSION_INT,
+};
+
+AVAndroidSurface *av_android_surface_new(void *listener, int tex_id)
+{
+    JNIEnv *env = NULL;
+    jobject *surface = NULL;
+    jobject *surface_texture = NULL;
+    jclass listener_class = NULL;
+    jmethodID listener_set_native_ptr_id = NULL;
+
+    AVAndroidSurface *ret = NULL;
+
+    ret = av_mallocz(sizeof(AVAndroidSurface));
+    if (!ret) {
+        return NULL;
+    }
+    ret->class = &android_surface_class;
+
+    pthread_mutex_init(&ret->lock, NULL);
+    pthread_cond_init(&ret->cond, NULL);
+
+    env = ff_jni_get_env(ret);
+    if (!env) {
+        av_freep(&ret);
+        return NULL;
+    }
+
+    if (ff_jni_init_jfields(env, &ret->jfields, jfields_mapping, 1, NULL) < 0) {
+        goto fail;
+    }
+
+    surface_texture = (*env)->NewObject(env,
+                                        ret->jfields.surface_texture_class,
+                                        ret->jfields.surface_texture_init_id,
+                                        tex_id);
+    if (!surface_texture) {
+        goto fail;
+    }
+
+    ret->surface_texture = (*env)->NewGlobalRef(env, surface_texture);
+    if (!ret->surface_texture) {
+        goto fail;
+    }
+    ret->tex_id = tex_id;
+
+    surface = (*env)->NewObject(env,
+                                ret->jfields.surface_class,
+                                ret->jfields.surface_init_id,
+                                ret->surface_texture);
+    if (!surface) {
+        goto fail;
+    }
+
+    ret->surface = (*env)->NewGlobalRef(env, surface);
+    if (!ret->surface) {
+        goto fail;
+    }
+
+    if (listener) {
+        ret->listener = (*env)->NewGlobalRef(env, listener);
+        if (!ret->listener) {
+            goto fail;
+        }
+
+        (*env)->CallVoidMethod(env,
+                               ret->surface_texture,
+                               ret->jfields.set_on_frame_available_listener_id,
+                               listener);
+        if (ff_jni_exception_check(env, 1, ret) < 0) {
+            goto fail;
+        }
+
+        listener_class = (*env)->GetObjectClass(env, listener);
+        if (ff_jni_exception_check(env, 1, ret) < 0) {
+            goto fail;
+        }
+
+        listener_set_native_ptr_id = (*env)->GetMethodID(env,
+                                                        listener_class,
+                                                        "setNativePtr",
+                                                        "(J)V");
+        if (ff_jni_exception_check(env, 1, ret) < 0) {
+            goto fail;
+        }
+
+        (*env)->CallVoidMethod(env, listener, listener_set_native_ptr_id, (jlong)ret);
+        if (ff_jni_exception_check(env, 1, ret) < 0) {
+            goto fail;
+        }
+    }
+
+    if (surface) {
+        (*env)->DeleteLocalRef(env, surface);
+    }
+    if (surface_texture) {
+        (*env)->DeleteLocalRef(env, surface_texture);
+    }
+    if (listener_class) {
+        (*env)->DeleteLocalRef(env, listener_class);
+    }
+
+    return ret;
+fail:
+    if (surface) {
+        (*env)->DeleteLocalRef(env, surface);
+    }
+
+    if (surface_texture) {
+        (*env)->DeleteLocalRef(env, surface_texture);
+    }
+
+    if (listener_class) {
+        (*env)->DeleteLocalRef(env, listener_class);
+    }
+
+    av_android_surface_free(&ret);
+
+    return NULL;
+}
+
+void av_android_surface_free(AVAndroidSurface **surface)
+{
+    JNIEnv *env = NULL;
+
+    if (!surface || !*surface) {
+        return;
+    }
+
+    env = ff_jni_get_env(*surface);
+    if (!env) {
+        av_freep(surface);
+        return;
+    }
+
+    if ((*surface)->surface) {
+        (*env)->CallVoidMethod(env, (*surface)->surface, (*surface)->jfields.surface_release_id);
+        if (ff_jni_exception_check(env, 1, surface) < 0) {
+            goto fail;
+        }
+    }
+
+    if ((*surface)->surface_texture) {
+        (*env)->CallVoidMethod(env, (*surface)->surface_texture, (*surface)->jfields.surface_texture_release_id);
+        if (ff_jni_exception_check(env, 1, surface) < 0) {
+            goto fail;
+        }
+    }
+
+fail:
+    if ((*surface)->surface) {
+        (*env)->DeleteGlobalRef(env, (*surface)->surface);
+        (*surface)->surface = NULL;
+    }
+
+    if ((*surface)->surface_texture) {
+        (*env)->DeleteGlobalRef(env, (*surface)->surface_texture);
+        (*surface)->surface_texture = NULL;
+    }
+
+    if ((*surface)->listener) {
+        (*env)->DeleteGlobalRef(env, (*surface)->listener);
+        (*surface)->listener = NULL;
+    }
+
+    ff_jni_reset_jfields(env, &(*surface)->jfields, jfields_mapping, 1, *surface);
+
+    pthread_mutex_destroy(&(*surface)->lock);
+    pthread_cond_destroy(&(*surface)->cond);
+    av_freep(surface);
+}
+
+
+void *av_android_surface_get_surface(AVAndroidSurface *surface)
+{
+    if (!surface) {
+        return NULL;
+    }
+
+    return surface->surface;
+}
+
+
+int av_android_surface_attach_to_gl_context(AVAndroidSurface *surface, int tex_id)
+{
+    int ret = 0;
+    JNIEnv *env = NULL;
+
+    if (!surface) {
+        return 0;
+    }
+
+    env = ff_jni_get_env(surface);
+    if (!env) {
+        av_freep(surface);
+        return AVERROR_EXTERNAL;
+    }
+
+    if (surface->tex_id != tex_id) {
+        av_android_surface_detach_from_gl_context(surface);
+    }
+
+    (*env)->CallVoidMethod(env,
+                           surface->surface_texture,
+                           surface->jfields.attach_to_gl_context_id,
+                           tex_id);
+    if (ff_jni_exception_check(env, 1, surface) < 0) {
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+    surface->tex_id = tex_id;
+
+fail:
+    return ret;
+}
+
+int av_android_surface_detach_from_gl_context(AVAndroidSurface *surface)
+{
+    int ret = 0;
+    JNIEnv *env = NULL;
+
+    if (!surface || surface->tex_id < 0) {
+        return 0;
+    }
+
+    env = ff_jni_get_env(surface);
+    if (!env) {
+        av_freep(surface);
+        return AVERROR_EXTERNAL;
+    }
+
+    (*env)->CallVoidMethod(env,
+                           surface->surface_texture,
+                           surface->jfields.detach_from_gl_context_id);
+    if (ff_jni_exception_check(env, 1, surface) < 0) {
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+    surface->tex_id = -1;
+
+fail:
+    return ret;
+}
+
+int av_android_surface_render_buffer(AVAndroidSurface *surface, AVMediaCodecBuffer *buffer, float *matrix)
+{
+    int ret = 0;
+    JNIEnv *env = NULL;
+    jfloatArray array = NULL;
+
+    if (!surface) {
+        return 0;
+    }
+
+    env = ff_jni_get_env(surface);
+    if (!env) {
+        av_freep(surface);
+        return AVERROR_EXTERNAL;
+    }
+
+    pthread_mutex_lock(&surface->lock);
+    surface->on_frame_available = 0;
+
+    if (av_mediacodec_release_buffer(buffer, 1) < 0) {
+        pthread_mutex_unlock(&surface->lock);
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+
+    if (surface->listener && !surface->on_frame_available) {
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+        ts.tv_nsec += 30000000;
+        pthread_cond_timedwait(&surface->cond, &surface->lock, &ts);
+    }
+
+    ret = surface->on_frame_available;
+    pthread_mutex_unlock(&surface->lock);
+
+    if (!ret) {
+        av_log(surface, AV_LOG_WARNING, "No frame available\n");
+    }
+
+    (*env)->CallVoidMethod(env, surface->surface_texture, surface->jfields.update_tex_image_id);
+    if (ff_jni_exception_check(env, 1, surface) < 0) {
+        pthread_mutex_unlock(&surface->lock);
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+
+    array = (*env)->NewFloatArray(env, 16);
+    if (!array) {
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+    (*env)->CallVoidMethod(env,
+                           surface->surface_texture,
+                           surface->jfields.get_transform_matrix_id,
+                           array);
+    if (ff_jni_exception_check(env, 1, surface) < 0) {
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+
+    (*env)->GetFloatArrayRegion(env, array, 0, 16, matrix);
+    if (ff_jni_exception_check(env, 1, surface) < 0) {
+        ret = AVERROR_EXTERNAL;
+        goto fail;
+    }
+
+fail:
+    if (array) {
+        (*env)->DeleteLocalRef(env, array);
+    }
+
+    return ret;
+}
+
+void av_android_surface_signal_frame(AVAndroidSurface *surface)
+{
+    if (!surface) {
+        return;
+    }
+
+    pthread_mutex_lock(&surface->lock);
+    surface->on_frame_available = 1;
+    pthread_cond_signal(&surface->cond);
+    pthread_mutex_unlock(&surface->lock);
+}
+
 #else
 
 #include <stdlib.h>
@@ -119,6 +507,39 @@ void av_mediacodec_default_free(AVCodecContext *avctx)
 int av_mediacodec_release_buffer(AVMediaCodecBuffer *buffer, int render)
 {
     return AVERROR(ENOSYS);
+}
+
+AVAndroidSurface *av_android_surface_new(void* listener, int tex_id)
+{
+    return NULL;
+}
+
+void av_android_surface_free(AVAndroidSurface **surface)
+{
+}
+
+void *av_android_surface_get_surface(AVAndroidSurface *surface)
+{
+    return NULL;
+}
+
+int av_android_surface_attach_to_gl_context(AVAndroidSurface *surface, int tex_id)
+{
+    return 0;
+}
+
+int av_android_surface_detach_from_gl_context(AVAndroidSurface *surface)
+{
+    return 0;
+}
+
+int av_android_surface_render_buffer(AVAndroidSurface *surface, AVMediaCodecBuffer *buffer, float *matrix)
+{
+    return 0;
+}
+
+void av_android_surface_signal_frame(AVAndroidSurface *surface)
+{
 }
 
 #endif
